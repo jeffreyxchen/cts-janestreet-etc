55ea5998a532f953fd977641642c6ce42d8193c6
Jeffrey Chen <jeffreychen@Jeffreys-MacBook-Pro.local>
2017-07-22 16:34:18 -0700

    add pennying

--- pennying.js
+++ pennying.js
@@ -1,6 +1,8 @@
 function penny(symbol, buyPrice, sellPrice) {
-  if(buyPrice < sellPrice) {
-    client.write(JSON.stringify({"type": "add", "order_id": new Date(), "symbol": symbol, "dir": "BUY", "price": buyPrice+0.01, "size": 1})+"\n");
-    client.write(JSON.stringify({"type": "add", "order_id": new Date(), "symbol": symbol, "dir": "SELL", "price": sellPrice-0.01, "size": 1})+"\n");
+  if(buyPrice - sellPrice > 0.02) {
+    client.write(JSON.stringify({"type": "add", "order_id": counter, "symbol": symbol, "dir": "BUY", "price": buyPrice+0.01, "size": 1})+"\n");
+    counter++;
+    client.write(JSON.stringify({"type": "add", "order_id": counter, "symbol": symbol, "dir": "SELL", "price": sellPrice-0.01, "size": 1})+"\n");
+    counter++;
   }
 }
