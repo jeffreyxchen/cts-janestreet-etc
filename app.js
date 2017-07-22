9e5e9c18ad654039e173f0d33f5601c5b7bafd86
alexthkim <atk3@rice.edu>
2017-07-22 12:56:53 -0700

    add

--- app.js
+++ app.js
@@ -2,14 +2,14 @@
 var net = require('net');
 
 var HOST = '10.0.49.161';
-var PORT = 25000;
+var PORT = 20000;
 
 var client = new net.Socket();
 client.connect(PORT, HOST, function() {
 
     console.log('CONNECTED TO: ' + HOST + ':' + PORT);
     // Write a message to the socket as soon as the client is connected, the server will receive it as message from the client
-    client.write(JSON.stringify({"type": "hello", "team": "CTS"}));
+    client.write("HELLO CTS");
 
 });
 
