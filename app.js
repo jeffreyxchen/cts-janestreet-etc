fe5cf9253c4df88018fd3ba9798a71f83ab86ac4
alexthkim <atk3@rice.edu>
2017-07-22 13:57:00 -0700

    what

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ var net = require('net');
 
 var TEST = '10.0.49.161';
 var PROD = '1.1.1.1';
-var PORT = 20000;
+var PORT = 25000;
 
 var client = new net.Socket();
 client.connect(PORT, PROD, function() {
 
     console.log('CONNECTED TO: ' + PROD + ':' + PORT);
     // Write a message to the socket as soon as the client is connected, the server will receive it as message from the client
-    client.write("HELLO CTS\n");
+    client.write(JSON.stringify({"type": "hello", "team": "CTS"}) + "\n");
 
 });
 
@@ -18,9 +18,9 @@ client.connect(PORT, PROD, function() {
 // data is what the server sent to this socket
 client.on('data', function(data) {
 
-    var buf = new Buffer(JSON.stringify(data), "utf-8");
-    var temp = JSON.parse(buf.toString());
-    console.log(temp);
+    // var buf = new Buffer(JSON.stringify(data), "utf-8");
+    // var temp = JSON.parse(buf.toString());
+    console.log(JSON.stringify(data));
     //var splitted = lines.split('\n');
 
     // splitted.forEach(function(line) {
