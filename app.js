b7311b8dfc7b0939a368d55b30a7962ecacd81cf
alexthkim <atk3@rice.edu>
2017-07-22 12:33:40 -0700

    new stuff

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
-var io = require('socket.io-client');
-var socket = io('http://10.0.49.161');
+var net = require('net');
 
-console.log("hello");
+var HOST = '10.0.49.161';
+var PORT = 6969;
 
-socket.on('connect', function(){
-  console.log('connected');
-})
+// Create a server instance, and chain the listen function to it
+// The function passed to net.createServer() becomes the event handler for the 'connection' event
+// The sock object the callback function receives UNIQUE for each connection
+net.createServer(function(sock) {
+
+    // We have a connection - a socket object is assigned to the connection automatically
+    console.log('CONNECTED: ' + sock.remoteAddress +':'+ sock.remotePort);
+
+    // Add a 'data' event handler to this instance of socket
+    sock.on('data', function(data) {
+
+        console.log('DATA ' + sock.remoteAddress + ': ' + data);
+        // Write the data back to the socket, the client will receive it as data from the server
+        sock.write('You said "' + data + '"');
+
+    });
+
+    // Add a 'close' event handler to this instance of socket
+    sock.on('close', function(data) {
+        console.log('CLOSED: ' + sock.remoteAddress +' '+ sock.remotePort);
+    });
+
+}).listen(PORT, HOST);
 
 //arbitrage stuff
