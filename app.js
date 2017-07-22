903ec7d9e03e76da001a8835c68e93de96263152
alexthkim <atk3@rice.edu>
2017-07-22 13:26:49 -0700

    ajsfda

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ client.connect(PORT, PROD, function() {
 // Add a 'data' event handler for the client socket
 // data is what the server sent to this socket
 client.on('data', function(data) {
-    console.log(jsoned.toString());
+    console.log(data.toString());
     //var splitted = data.split('\n');
 
     // splitted.forEach(function(line) {
