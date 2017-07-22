63b93052aca4b05d1c2423372668e5ee1bf25a72
Jeffrey Chen <jeffreychen@Jeffreys-MacBook-Pro.local>
2017-07-22 14:51:38 -0700

    end point ADR

--- arbitrageADR.js
+++ arbitrageADR.js
@@ -1,6 +1,5 @@
-{"type": "hello", "team": "CTS"}
-
 //nokus = stock, nokfh = ADR, conversion rate = 10/conversion
+
 function doNOKUSArbitrage (nokus, nokfh) {
   var index = 0;
   while (index < 100) {
@@ -8,49 +7,47 @@ function doNOKUSArbitrage (nokus, nokfh) {
     amtNOKUS = amt * nokus;
     valNOKUS = amtNOKUS + 10;
     if (valNOKUS < nokfh) {
-      //send the following obj
-      client.write({"type": "convert", "order_id": index+new Date(), "symbol": "NOKFH", "dir": "BUY", "size": 1});
-      client.write({"type": "add", "order_id": index+new Date(), "symbol": "NOKUS", "dir": "SELL", "size": 1});
-    } else {
+      client.write(JSON.stringify({"type": "add", "order_id": index, "symbol": "NOKUS", "dir": "BUY", "price": nokfh, "size": 5})+"\n");
+      client.write(JSON.stringify({"type": "convert", "order_id": index, "symbol": "NOKUS", "dir": "SELL", "size": 5})+"\n");
+      client.write(JSON.stringify({"type": "add", "order_id": index, "symbol": "NOKFH", "dir": "SELL", "price": nokus, "size": 5})+"\n");
+    }
+    else {
       var tempAMT = amt;
       while ((nokus*tempAMT+10)/tempAMT < nokfh) {
         tempAMT++;
       }
-      if(tempAMT > 20) {
+      if(tempAMT > 10) {
         return;
       }
       else {
-        //send the following obj
-        client.write({"type": "convert", "order_id": index+new Date(), "symbol": "NOKFH", "dir": "BUY", "size": 2*tempAMT});
-        client.write({"type": "add", "order_id": index+new Date(), "symbol": "NOKUS", "dir": "SELL", "size": 2*tempAMT});
+        client.write(JSON.stringify({"type": "add", "order_id": index, "symbol": "NOKUS", "dir": "BUY", "price": nokus, "size": 2*tempAMT})+"\n");
+        client.write(JSON.stringify({"type": "convert", "order_id": index, "symbol": "NOKUS", "dir": "SELL", "size": 2*tempAMT})+"\n");
+        client.write(JSON.stringify({"type": "add", "order_id": index, "symbol": "NOKFH", "dir": "SELL", "price": nokfh, "size": 2*tempAMT})+"\n");
       }
     }
   }
   index++;
 }
-}
 
-doNOKUSArbitrage(90, 100);
-
-// function doNOKFHArbitrage (nokus, nokfh) {
-//   var amt = 1;
-//   amtNOKFH = amt * nokus;
-//   valNOKFH = amtNOKFH + 10;
-//   if (valNOKFH < nokus) {
-//     //send the following obj
-//     {"type": "convert", "order_id": /*insert number*/, "symbol": "NOKUS", "dir": "BUY", "size": 1};
-//     {"type": "add", "order_id": /*insert number*/, "symbol": "NOKFH", "dir": "SELL", "size": 1};
-//   } else {
-//     var tempAMT = amt;
-//     while ((nokus*tempAMT+10)/tempAMT < nokfh) {
-//       tempAMT++;
-//     }
-//     if(tempAMT > 20) {
-//       return;
-//       else {
-//         //send the following obj
-//         {"type": "convert", "order_id": /*insert number*/, "symbol": "NOKUS", "dir": "BUY", "size": 2*tempAMT};
-//         {"type": "add", "order_id": /*insert number*/, "symbol": "NOKFH", "dir": "SELL", "size": 2*tempAMT};
-//       }
-//     }
-//   }
+function doNOKFHArbitrage (nokus, nokfh) {
+  var amt = 1;
+  amtNOKFH = amt * nokfh;
+  valNOKFH = amtNOKFH + 10;
+  if (valNOKFH < nokus) {
+    client.write(JSON.stringify({"type": "add", "order_id": index, "symbol": "NOKFH", "dir": "BUY", "price": nokfh, "size": 5})+"\n");
+    client.write(JSON.stringify({"type": "convert", "order_id": index, "symbol": "NOKUS", "dir": "BUY", "size": 5})+"\n");
+    client.write(JSON.stringify({"type": "add", "order_id": index, "symbol": "NOKUS", "dir": "SELL", "price": nokus, "size": 5})+"\n");
+  } else {
+    var tempAMT = amt;
+    while ((nokus*tempAMT+10)/tempAMT < nokfh) {
+      tempAMT++;
+    }
+    if(tempAMT > 10) {
+      return;
+      else {
+        client.write(JSON.stringify({"type": "add", "order_id": index, "symbol": "NOKFH", "dir": "BUY", "price": nokfh, "size": 2*tempAMT})+"\n");
+        client.write(JSON.stringify({"type": "convert", "order_id": index, "symbol": "NOKUS", "dir": "BUY", "size": 2*tempAMT})+"\n");
+        client.write(JSON.stringify({"type": "add", "order_id": index, "symbol": "NOKUS", "dir": "SELL", "price": nokus, "size": 2*tempAMT})+"\n");
+      }
+    }
+  }
