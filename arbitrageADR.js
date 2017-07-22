681351618c0d70500f75c81b1fb81baf741808f3
Jeffrey Chen <jeffreychen@Jeffreys-MacBook-Pro.local>
2017-07-22 12:27:36 -0700

    add ADR arbitrage

---
+++ arbitrageADR.js
@@ -0,0 +1,47 @@
+
+//nokus = stock, nokfh = ADR, conversion rate = 10/conversion
+function doNOKUSArbitrage (nokus, nokfh) {
+  var amt = 1;
+  amtNOKUS = amt * nokus;
+  valNOKUS = amtNOKUS + 10;
+  if (valNOKUS < nokfh) {
+    //send the following obj
+    {"type": "add", "order_id": /*insert number*/, "symbol": "NOKFH", "dir": "BUY", "size": 1};
+    {"type": "add", "order_id": /*insert number*/, "symbol": "NOKUS", "dir": "SELL", "size": 1};
+  } else {
+    var tempAMT = amt;
+    while ((nokus*tempAMT+10)/tempAMT < nokfh) {
+      tempAMT++;
+    }
+    if(tempAMT > 20) {
+      return;
+    else {
+      //send the following obj
+      {"type": "add", "order_id": /*insert number*/, "symbol": "NOKFH", "dir": "BUY", "size": 2*tempAMT};
+      {"type": "add", "order_id": /*insert number*/, "symbol": "NOKUS", "dir": "SELL", "size": 2*tempAMT};
+    }
+  }
+}
+
+function doNOKFHArbitrage (nokus, nokfh) {
+  var amt = 1;
+  amtNOKFH = amt * nokus;
+  valNOKFH = amtNOKFH + 10;
+  if (valNOKFH < nokus) {
+    //send the following obj
+    {"type": "add", "order_id": /*insert number*/, "symbol": "NOKUS", "dir": "BUY", "size": 1};
+    {"type": "add", "order_id": /*insert number*/, "symbol": "NOKFH", "dir": "SELL", "size": 1};
+  } else {
+    var tempAMT = amt;
+    while ((nokus*tempAMT+10)/tempAMT < nokfh) {
+      tempAMT++;
+    }
+    if(tempAMT > 20) {
+      return;
+    else {
+      //send the following obj
+      {"type": "add", "order_id": /*insert number*/, "symbol": "NOKUS", "dir": "BUY", "size": 2*tempAMT};
+      {"type": "add", "order_id": /*insert number*/, "symbol": "NOKFH", "dir": "SELL", "size": 2*tempAMT};
+    }
+  }
+}
