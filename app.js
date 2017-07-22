f4d3e79c154dcd2a84c291d878f68d15b951864b
alexthkim <atk3@rice.edu>
2017-07-22 13:09:34 -0700

    connection to server

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 //arbitrage stuff
 var net = require('net');
 
-var HOST = '10.0.49.161';
+var TEST = '10.0.49.161';
+var PROD = '1.1.1.1';
 var PORT = 20000;
 
 var client = new net.Socket();
 client.connect(PORT, HOST, function() {
 
-    console.log('CONNECTED TO: ' + HOST + ':' + PORT);
+    console.log('CONNECTED TO: ' + PROD + ':' + PORT);
     // Write a message to the socket as soon as the client is connected, the server will receive it as message from the client
     client.write("HELLO CTS\n");
 
