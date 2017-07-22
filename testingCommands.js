d6d29ba47fa63e5bcb5cc542dec91c7f940ca48d
Jeffrey Chen <jeffreychen@Jeffreys-MacBook-Pro.local>
2017-07-22 14:20:36 -0700

    consolidate 1 line

--- testingCommands.js
+++ testingCommands.js
@@ -1,6 +1,6 @@
 
 {"type": "hello", "team": "CTS"}
-{"type": "add", "order_id": N, "symbol": "BOND", "dir": "BUY", "price": 999, "size": 10}
+{"type": "add", "order_id": 1, "symbol": "BOND", "dir": "BUY", "price": 1005, "size": 10}
 
 
 {"type": "hello", "team": "CTS"}
