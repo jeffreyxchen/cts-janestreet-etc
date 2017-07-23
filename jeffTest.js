14f93d8ab64af77f1dd27aeb0e6ca13656475c17
alexthkim <atk3@rice.edu>
2017-07-22 19:48:18 -0700

    ty amy

--- jeffTest.js
+++ jeffTest.js
@@ -115,18 +115,18 @@ client.on('data', function(data) {
         client.write(JSON.stringify({"type": "add", "order_id": counter, "symbol": symbol, "dir": "BUY", "price": fairValue + 1, "size": 5}) + "\n")
         counter++;
         console.log(canceler[pennyIdx*2])
-        canceler[pennyIdx*2].append(fairValue);
+        canceler[pennyIdx*2].push(fairValue);
         canceler[pennyIdx*2].splice(0,1);
       } else {
         client.write(JSON.stringify({"type": "add", "order_id": counter, "symbol": symbol, "dir": "SELL", "price": fairValue - 1, "size": 5}) + "\n")
         counter++;
         console.log(canceler[pennyIdx*2])
-        canceler[pennyIdx*2].append(fairValue);
+        canceler[pennyIdx*2].push(fairValue);
         canceler[pennyIdx*2].splice(0,1);
       }
     } else {
       console.log(canceler[pennyIdx*2])
-      canceler[pennyIdx*2].append(fairValue);
+      canceler[pennyIdx*2].push(fairValue);
       canceler[pennyIdx*2 + 1]++;
     }
 
