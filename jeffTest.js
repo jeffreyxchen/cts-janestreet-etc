707525466a5b9ade42631b48faba7c771b20b0b9
Jeffrey Chen <jeffreychen@Jeffreys-MacBook-Pro.local>
2017-07-22 17:20:10 -0700

    add logs

--- jeffTest.js
+++ jeffTest.js
@@ -88,10 +88,12 @@ client.on('data', function(data) {
 
   function penny(symbol, buyPrice, sellPrice) {
     if(buyPrice - sellPrice > 0.02) {
+      console.log('test');
       client.write(JSON.stringify({"type": "add", "order_id": counter, "symbol": symbol, "dir": "BUY", "price": buyPrice+0.01, "size": 1})+"\n");
       counter++;
       client.write(JSON.stringify({"type": "add", "order_id": counter, "symbol": symbol, "dir": "SELL", "price": sellPrice-0.01, "size": 1})+"\n");
       counter++;
+      console.log("DOES IT GET HERE??");
     }
   }
 
