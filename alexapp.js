2613e0c6a19476275adc484b190e7d80e66896cd
alexthkim <atk3@rice.edu>
2017-07-22 19:57:09 -0700

    ahh

--- alexapp.js
+++ alexapp.js
@@ -13,7 +13,7 @@ var nokus_buy = 0;
 var nokus_sell = 0;
 var counter = 0;
 
-client.connect(PORT, TEST, function() {
+client.connect(PORT, PROD, function() {
 
   console.log('CONNECTED TO: ' + TEST + ':' + PROD);
   // Write a message to the socket as soon as the client is connected, the server will receive it as message from the client
--- jeffTest.js
+++ jeffTest.js
@@ -21,7 +21,7 @@ var pennyAAPLsell = 0, pennyAAPLbuy = 0;
 var pennyMSFTsell = 0, pennyMSFTbuy = 0;
 var pennyGOOGsell = 0, pennyGOOGbuy = 0;
 
-client.connect(PORT, TEST, function() {
+client.connect(PORT, PROD, function() {
 
   console.log('CONNECTED TO: ' + TEST + ':' + PROD);
   // Write a message to the socket as soon as the client is connected, the server will receive it as message from the client
